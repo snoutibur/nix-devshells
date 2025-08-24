15a44c3ddaed710cd5c341733f1b73ac0c918162
snoutibur <157987813+snoutibur@users.noreply.github.com>
2025-08-23 22:43:15 -0600

    [WIP] Java devshell
    Signed-off-by: snoutibur <157987813+snoutibur@users.noreply.github.com>

---
+++ java/flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "Java dev env";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=25.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, nixpkgs-unstable, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+
+        pkgs-unstable = import nixpkgs-unstable {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            # Java development
+            maven
+            gradel
+            openjdk21
+
+            # editor
+            pkgs-unstable.jetbrains.idea-ultimate
+            eclipses.eclipse-java
+          ];
+
+          shellHook = ''
+            idea-ultimate > /dev/null 2>&1 &
+            echo "Java dev shell loaded"
+          '';
+        };
+      }
+    );
+}
